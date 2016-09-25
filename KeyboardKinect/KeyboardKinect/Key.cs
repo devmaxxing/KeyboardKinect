d41c6976d4cfe15911ef96e9195afb91be34db3b
oliverfei <ofei006@uottawa.ca>
2016-09-24 21:27:01 -0400

    Removed some unused libraries. Added Key class.

---
+++ KeyboardKinect/KeyboardKinect/Key.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace KeyboardKinect
+{
+    class Key
+    {
+        Rectangle rect;
+        KeyReg keyReg;
+        List<int> pixelIndices;
+
+        public Key(Rectangle r, KeyReg k, List<int> l)
+        {
+            rect = r;
+            keyReg = k;
+            pixelIndices = l;
+        }
+    }
+}
--- KeyboardKinect/KeyboardKinect/MainWindow.xaml.cs
+++ KeyboardKinect/KeyboardKinect/MainWindow.xaml.cs
@@ -11,30 +11,35 @@ using System;
 
 namespace KeyboardKinect
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-        private KinectSensor _sensor = null;
-        private ColorFrameReader _colorReader = null;
-        private DepthFrameReader _depthReader = null;
-        private List<ushort[]> calibrationFrameData = null;
-        private List<Rectangle> keys;
-        private bool calibrating = false;
+        private List<Key> keys;
+
+        private bool calibrating;
+        private bool detecting;
 
         int frameWidth;
         int frameHeight;
 
-        /// <summary>
-        /// The main window of the app.
-        /// </summary>
+        private ushort[] calib;
+
+        private KinectSensor _sensor;
+        private DepthFrameReader _depthReader;
+        
         public MainWindow()
         {
             InitializeComponent();
 
+            keys = new List<Key>();
+
+            calibrating = false;
+            detecting = false;
+
             frameWidth = 512;
             frameHeight = 424;
+
+            calib = new ushort[frameWidth * frameHeight];
 
             _sensor = KinectSensor.GetDefault();
 
@@ -49,18 +54,22 @@ namespace KeyboardKinect
 
         private void DepthReader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
         {
-            canvas.Children.Clear();
-
             using (DepthFrame frame = e.FrameReference.AcquireFrame())
             {
                 if (frame != null)
                 {
                     if (calibrating)
                     {
-                        
+                        frame.CopyFrameDataToArray(calib);
                         calibrating = false;
                     }
-                    //camera.Source = frame.ToBitmap();
+                    if (detecting)
+                    {
+
+                    }else
+                    {
+                        camera.Source = ToBitmap(frame);
+                    }                    
                 }
             }
         }
@@ -72,12 +81,6 @@ namespace KeyboardKinect
             {
                 _depthReader.Dispose();
                 _depthReader = null;
-            }
-
-            if (_colorReader != null)
-            {
-                _colorReader.Dispose();
-                _colorReader = null;
             }
 
             if (_sensor != null)
@@ -92,15 +95,72 @@ namespace KeyboardKinect
             
         }
 
-        private bool hitDetected(ushort[] calib, ushort[] frame)
+        private List<int> getPixelIndices(Rectangle r)
         {
-            for(var i = 0; i<calib.Length; i++)
+            List<int> keyPixels = new List<int>();
+            for(var x = 0; x<frameWidth; x++)
+            {
+                for(var y = 0; y<frameWidth; y++)
+                {
+                    HitTestResult result = VisualTreeHelper.HitTest(canvas, new Point(x, y));
+                    if (result != null)
+                    {
+                        keyPixels.Add(y * frameWidth + x);
+                    }
+                }                
+            }
+            return keyPixels;
+        }
+
+        private ImageSource ToBitmap(DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] depthData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(depthData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex)
+            {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixelData, stride);
+        }
+
+        private bool hitDetected(ushort[] keyPixels, ushort[] frame)
+        {
+            foreach(int i in keyPixels)
             {
                 var difference = calib[i] - frame[i];
-                if (difference < 10 && difference > 5)
+                if (difference < 20 && difference > 10)
                     return true;
             }
             return false;
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            detecting = !detecting;
+            if (detecting)
+            {
+                camera.Source = null;
+            }
         }
     }
 }
